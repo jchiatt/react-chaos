f52d6d13e56a288d282fcbc0710cb2ce1168659b
J.C. Hiatt <jc@jchiatt.com>
2019-05-21 07:34:18 -0500

    temporarily move library code inside of example app until I figure out how to correctly do module exports

--- react-chaos-example/src/App.js
+++ react-chaos-example/src/App.js
@@ -1,24 +1,41 @@
 import React from 'react';
+import Chaos from './react-chaos'
 
 function App() {
   return (
     <>
-      <ComponentOne />
-      <ComponentTwo />
-      <ComponentThree />
+      <Chaos level={1}>
+        <ComponentOne />
+        <ComponentTwo />
+        <ComponentThree />
+      </Chaos>
     </>
   );
 }
 
+function GenericComponent({ text }) {
+  return <h1>Component {text}</h1>
+}
+
+function NestedComponent({ children }) {
+  return children;
+}
+
 function ComponentOne() {
-  return <h1>Component One</h1>
+  return <GenericComponent text="One" />
 }
 
 function ComponentTwo() {
-  return <h1>Component Two</h1>
+  return <GenericComponent text="Two" />
 }
+
 function ComponentThree() {
-  return <h1>Component Three</h1>
+  return (
+    <NestedComponent>
+      <GenericComponent text="Three" />
+      <GenericComponent text="Four" />
+    </NestedComponent>
+  )
 }
 
 export default App;
