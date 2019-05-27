cc753a1d0aa7fb0af0d472e24485b2994ebcb501
J.C. Hiatt <jc@jchiatt.com>
2019-05-27 17:29:00 -0500

    WIP jest test

--- __tests__/index-test.js
+++ __tests__/index-test.js
@@ -1,24 +1,11 @@
 import expect from 'expect'
-import React from 'react'
-import {render, unmountComponentAtNode} from 'react-dom'
+import { createChaos } from '../src/utils/createChaos'
 
-describe('Chaos renders', () => {
-  let node
-  const TestComponent = () => <h1>Test</h1>
-
-
-  beforeEach(() => {
-    node = document.createElement('div')
-  })
-
-  afterEach(() => {
-    unmountComponentAtNode(node)
-  })
-
-  it('always passes this test', () => {
-    render(<TestComponent />, node, () => {
-      expect(node.innerHTML).toContain('Test')
-    })
+describe('createChaos function', () => {
+  const result = () => createChaos();
+  
+  it('can be called', () => {
+    expect(result).toThrow()
   })
 })
 
