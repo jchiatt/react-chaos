c6da859510da1b1f9f3609da7d15c6cc47aaac82
J.C. Hiatt <jc@jchiatt.com>
2019-05-27 13:52:21 -0500

    extend withChaos to optionally accept a runInProduction parameter

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Chaos } from './components/Chaos'
 
-const withChaos = (WrappedComponent, level, errorMessage) => {
-  if (process.env.NODE_ENV === 'production') {
+const withChaos = (WrappedComponent, level, errorMessage, runInProduction = false) => {
+  if (process.env.NODE_ENV === 'production' && !runInProduction) {
     console.warn(`You tried to use React Chaos in production. You probably didn't mean to do this. Chaos will not occur in production.`);
     return () => <WrappedComponent />
   }
