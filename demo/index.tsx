c39b3a369f52be7d486d3a4dde9217c5654d662a
J.C. Hiatt <jc@jchiatt.com>
2019-09-07 17:45:53 -0500

    chore(demo): remove withChaos higher order component from demo

--- demo/index.tsx
+++ demo/index.tsx
@@ -1,23 +1,22 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { withChaos, initChaos } from '../src/index';
+import { initChaos } from '../src/index';
 initChaos(React);
 import ErrorBoundary from '../src/components/ErrorBoundary';
 import './index.css';
 
 function Demo() {
   return (
-    <React.Fragment>
+    <>
       <div className="wrapper">
         <div className="container child one">
-          <ComponentOne />
           <ErrorBoundary fallback={<Fallback />}>
-            <ComponentWithChaos />
+            <ComponentOne />
           </ErrorBoundary>
         </div>
       </div>
-    </React.Fragment>
+    </>
   );
 }
 
@@ -30,21 +29,12 @@ const Fallback = () => (
       fontWeight: 700,
     }}
   >
-    <span> I'm handling the error gracefully. 💅 </span>
+    <span>I'm handling the error gracefully. 💅</span>
   </div>
 );
 
 const Text = ({ text }) => <pre>Component {text}</pre>;
 
 const ComponentOne = () => <Text text="One" />;
-
-const ComponentMayHaveChaos = () => <Text text="may experience chaos." />;
-
-const ComponentWithChaos = withChaos(
-  ComponentMayHaveChaos,
-  5,
-  'a custom error message, level 5',
-  true
-);
 
 ReactDOM.render(<Demo />, document.querySelector('#root'));
